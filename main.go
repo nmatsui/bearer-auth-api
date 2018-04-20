81fb47c640e601a72ec686aacc5b2a4d157f8c4e
Nobuyuki Matsui <nobuyuki.matsui@gmail.com>
2018-04-20 12:38:51 +0900

    add tests for main package

--- main.go
+++ main.go
@@ -2,18 +2,28 @@ package main
 
 import (
 	"os"
+	"strconv"
 
 	"github.com/nmatsui/bearer-auth-api/router"
 )
 
+const LISTEN_PORT = "LISTEN_PORT"
 const DEFAULT_PORT = "8080"
 
 func main() {
-	port := os.Getenv("LISTEN_PORT")
+	handler := router.NewHandler()
+	handler.Run(getListenPort())
+}
+
+func getListenPort() string {
+	port := os.Getenv(LISTEN_PORT)
 	if len(port) == 0 {
 		port = DEFAULT_PORT
 	}
+	intPort, err := strconv.Atoi(port)
+	if err != nil || intPort < 1 || 65535 < intPort {
+		port = DEFAULT_PORT
+	}
 
-	handler := router.NewHandler()
-	handler.Run(":" + port)
+	return ":" + port
 }
